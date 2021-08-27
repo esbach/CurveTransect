e0927aa26d97386777205d0ccae9c38445f5ec0e
esbach <michael.s.esbach@gmail.com>
2021-08-27 11:08:44 -0400

    updated documentation

--- R/CurveTransect-package.R
+++ R/CurveTransect-package.R
@@ -1,26 +1,24 @@
 #' Curving Transect Distance Sampling
 #'
 #' `CurveTransect` facilitates distance sampling on a curving transect.
-#'
+#' 
+#' `observerXY`: creates points on a transect at specific meter intervals
+#' `objectXY`: provides spatial coordinates for each detected object in a distance sampling survey
+#' `cca`: calculates the an adjusted transect length for curved covered area calculations
+#' 
 #' Examples of how to use this package are available at
 #' https://github.com/esbach/CurveTransect.
 #'
 #' For help with distance sampling, there is a Google Group
 #' https://groups.google.com/forum/#!forum/distance-sampling.
 #'
-#' @name CurveTransect
-#' @import geosphere
-#' @docType package
-#' @author Michael Esbach <mesbach@@princeton.edu>
-#' @section CurveTransect Functions
-#' 
-#' `observerXY`: creates points on a transect at specific meter intervals
-#' `objectXY`: provides spatial coordinates for each detected object in a distance sampling survey
-#' `cca`: calculates the an adjusted transect length for curved covered area calculations
-#' 
+#' @author Michael Esbach <mesbach@@princeton.edu>#' 
 #' @references
 #'
 #' Esbach M.S. and R.K. Patra. In review. Distance Sampling from Curving Transects in Dense Forests.
 #'   Methods in Ecology and Evolution.
+#' 
+#' @name CurveTransect
+#' @docType package
+#' @import geosphere
 NULL
-#> NULL
